6091415dd625b46e73a2d6c7f4aa0767167ee939
lcsoares10 <lucassoares81@hotmail.com>
2021-03-07 13:58:43 -0300

    feat: creating dynamic page through the slug using Page template

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,40 @@
+import client from 'graphql/client'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/query'
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/dist/client/router'
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 1 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+  // fallback: true , caso o slug não tenha sido mapeado , e tenha uma solicitção para path não mapeado o fallback true , ira mapear o path em realtime
+  return { paths, fallback: true }
+  console.log(pages)
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (!page) return { notFound: true }
+
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  }
+}
+
+export default function Page({ heading, body }: PageTemplateProps) {
+  const router = useRouter()
+
+  //retorna um loading , qq coisa enquanto ta sendo criada
+  if (router.isFallback) return null
+
+  return <PageTemplate heading={heading} body={body} />
+}
--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -2,21 +2,23 @@ import LinkWrapper from 'components/LinkWrapper'
 import { CloseCircle } from '@styled-icons/evaicons-solid/CloseCircle'
 import * as S from './styles'
 
-const AboutTemplate = () => {
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => {
   return (
     <S.Content>
       <LinkWrapper href="/">
         <CloseCircle size={32} arial-label="close" />
       </LinkWrapper>
-      <S.Heading>Nossas Viagens</S.Heading>
+      <S.Heading>{heading}</S.Heading>
       <S.Body>
-        <p>
-          O Intuito dessa página é mostrar todos as viagens que eu e minha
-          esposa fizemos pelo mundo
-        </p>
+        <div dangerouslySetInnerHTML={{ __html: body }} />
       </S.Body>
     </S.Content>
   )
 }
 
-export default AboutTemplate
+export default PageTemplate
